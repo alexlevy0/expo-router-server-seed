a4d5bcb798a8eb49381fca74a8bdbb550824d596
Alex Lévy <alexlevy0@gmail.com>
2024-05-11 19:33:29 +0200

    Add remix-run/node

--- app/generate+api.tsx
+++ app/generate+api.tsx
@@ -1,27 +1,32 @@
-import { ExpoRequest, ExpoResponse } from "expo-router/server"
+import { ExpoRequest, ExpoResponse } from "expo-router/server";
 
 export async function GET(req: ExpoRequest): Promise<ExpoResponse> {
-        return ExpoResponse.json({ message: "Hello, world" })
+	return ExpoResponse.json({ message: "Hello, world" });
 }
 
 export async function POST(req: ExpoRequest): Promise<ExpoResponse> {
-        const { prompt } = await req.json()
-        console.log("prompt:", prompt)
+	const { prompt } = await req.json();
+	console.log("prompt:", prompt);
 
-        const payload = {
-                prompt,
-                max_tokens: 100, // You can customize this
-        }
+	const payload = {
+		prompt,
+		max_tokens: 100, // You can customize this
+	};
 
-        const json = await fetch("https://api.openai.com/v1/engines/gpt-3.5-turbo-instruct/completions", {
-                headers: {
-                        "Content-Type": "application/json",
-                        Authorization: `Bearer ${process.env.OPENAI_API_KEY ?? ""}`,
-                },
-                method: "POST",
-                body: JSON.stringify(payload),
-        }).then((res) => res.json())
+	const json = await fetch(
+		"https://api.openai.com/v1/engines/gpt-3.5-turbo-instruct/completions",
+		{
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: `Bearer ${
+					process.env.OPENAI_API_KEY ?? ""
+				}`,
+			},
+			method: "POST",
+			body: JSON.stringify(payload),
+		}
+	).then((res) => res.json());
 
-        console.log(JSON.stringify(json, null, 2))
-        return ExpoResponse.json(json)
+	console.log(JSON.stringify(json, null, 2));
+	return ExpoResponse.json(json);
 }
