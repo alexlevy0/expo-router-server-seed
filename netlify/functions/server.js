fad6b4751a0c4c41daad56e59b693889dd8cdaf5
Alex Lévy <alexlevy0@gmail.com>
2024-05-12 19:03:45 +0200

    use netlify adapter

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,7 +1,8 @@
-const { createRequestHandler } = require("./netlify-adapter");
+
+const { createRequestHandler } = require('@expo/server/adapter/netlify');
 
 const handler = createRequestHandler({
-  build: require("path").join(__dirname, "../../dist"),
+  build: require('path').join(__dirname, '../../dist/server'),
   mode: process.env.NODE_ENV,
 });
 
